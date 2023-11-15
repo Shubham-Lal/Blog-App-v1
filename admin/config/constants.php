5b5002462c29380f3448f8b0df053a2afacca5b5
Shubham Lal <im.shubhamlal@gmail.com>
2023-11-16 01:10:51 +0530

    env setup in admin folder

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,3 +1,7 @@
 <?php
-define("ROOT_URL", "http://localhost:3000/");
+define("ROOT_URL", $_ENV["ROOT_URL"]);
+define("DB_HOST", $_ENV["DB_HOST"]);
+define("DB_USER", $_ENV["DB_USER"]);
+define("DB_PASS", $_ENV["DB_PASS"]);
+define("DB_NAME", $_ENV["DB_NAME"]);
 ?>
