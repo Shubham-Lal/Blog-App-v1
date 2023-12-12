8b36993df13c3a67182874fb4bade1cdbe4c8243
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-12 21:20:04 +0530

    admin: display users dynamically

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -1,8 +1,21 @@
 <?php
 include './partials/header.php';
+
+$current_admin_id = $_SESSION['user-id'];
+$query = "SELECT * FROM users WHERE NOT id=$current_admin_id";
+$users = mysqli_query($connection, $query);
 ?>
 
 <section class="dashboard">
+    <?php if (isset($_SESSION['add-user-success'])): ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['add-user-success'];
+                unset($_SESSION['add-user-success']);
+                ?>
+            </p>
+        </div>
+    <?php endif ?>
     <div class="container dashboard__container">
         <button class="sidebar__toggle" id="show__sidebar-btn">
             <i class="uil uil-angle-right-b"></i>
@@ -65,27 +78,15 @@ include './partials/header.php';
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>John Doe</td>
-                        <td>johndoe</td>
-                        <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-user.php" class="btn sm danger">Delete</a></td>
-                        <td>Yes</td>
-                    </tr>
-                    <tr>
-                        <td>Mary</td>
-                        <td>itzmary</td>
-                        <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-user.php" class="btn sm danger">Delete</a></td>
-                        <td>No</td>
-                    </tr>
-                    <tr>
-                        <td>Lily Jones</td>
-                        <td>lilyjones</td>
-                        <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-                        <td><a href="delete-user.php" class="btn sm danger">Delete</a></td>
-                        <td>No</td>
-                    </tr>
+                    <?php while ($user = mysqli_fetch_assoc($users)): ?>
+                        <tr>
+                            <td><?= "{$user['firstname']} {$user['lastname']}" ?></td>
+                            <td><?= $user['username'] ?></td>
+                            <td><a href="<?= ROOT_URL ?>admin/edit-user.php?id=<?= $user['id'] ?>" class="btn sm">Edit</a></td>
+                            <td><a href="<?= ROOT_URL ?>admin/delete-user.php?id=<?= $user['id'] ?>" class="btn sm danger">Delete</a></td>
+                            <td><?= $user['is_admin'] ? 'Yes' : 'No' ?></td>
+                        </tr>
+                    <?php endwhile ?>
                 </tbody>
             </table>
         </main>
