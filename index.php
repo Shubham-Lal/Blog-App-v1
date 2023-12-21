c9b5a18d29e7cbfdfd6b29b020b00d4a3e92e4d7
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-21 22:33:36 +0530

    dynamic Featured blog data

--- index.php
+++ index.php
@@ -1,39 +1,66 @@
 <?php
 include './partials/header.php';
+
+// Fetch featured post
+$featured_query = "SELECT * FROM posts where is_featured=1 LIMIT 1";
+$featured_result = mysqli_query($connection, $featured_query);
+$featured = mysqli_fetch_assoc($featured_result);
+
 ?>
 
 <!-- FEATURED POST -->
-<section class="featured">
-    <div class="container featured__container">
-        <div class="post__thumbnail">
-            <img src="./images/blog1.jpg" alt="Blog" />
-        </div>
-        <div class="post__info">
-            <a href="category-posts.php" class="category__button">Street Art</a>
-            <h2 class="post__title">
-                <a href="post.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis?</a>
-            </h2>
-            <p class="post__body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, iusto! Ex repellendus
-                obcaecati quod, voluptas quasi, tempore consequuntur rerum maiores iure excepturi provident eligendi
-                dicta deserunt, nobis a repellat incidunt?
-            </p>
-            <div class="post__author">
-                <div class="post__author-avatar">
-                    <img src="./images/avatar12.jpg" alt="Author Avatar">
-                </div>
-                <div class="post__author-info">
-                    <h5>By: Jane Doe</h5>
-                    <small>November 13, 2023 - 12:29</small>
+<?php if (mysqli_num_rows($featured_result) == 1): ?>
+    <section class="featured">
+        <div class="container featured__container">
+            <div class="post__thumbnail">
+                <img src="<?= ROOT_URL . "images/" . $featured['thumbnail'] ?>" alt="Blog" />
+            </div>
+            <div class="post__info">
+                <?php
+                $category_id = $featured['category_id'];
+                $category_query = "SELECT * FROM categories where id=$category_id LIMIT 1";
+                $category_result = mysqli_query($connection, $category_query);
+                $category = mysqli_fetch_assoc($category_result);
+                ?>
+                <a href="<?= ROOT_URL . 'category-posts.php?id=' . $category['id'] ?>" class="category__button">
+                    <?= $category['title'] ?>
+                </a>
+                <h2 class="post__title">
+                    <a href="<?= ROOT_URL . 'post.php?id=' . $featured['id'] ?>">
+                        <?= $featured['title'] ?>
+                    </a>
+                </h2>
+                <p class="post__body">
+                    <?= substr($featured['body'], 0, 500) ?>
+                    <?= strlen($featured['body']) > 500 ? "..." : "" ?>
+                </p>
+                <div class="post__author">
+                    <?php
+                    $author_id = $featured['author_id'];
+                    $author_query = "SELECT * FROM users where id=$author_id LIMIT 1";
+                    $author_result = mysqli_query($connection, $author_query);
+                    $author = mysqli_fetch_assoc($author_result);
+                    ?>
+                    <div class="post__author-avatar">
+                        <img src="<?= ROOT_URL . 'images/' . $author['avatar'] ?>" alt="Author Avatar">
+                    </div>
+                    <div class="post__author-info">
+                        <h5>
+                            <?= 'By: ' . $author['firstname'] . $author['lastname'] ?>
+                        </h5>
+                        <small>
+                            <?= date("M d, Y - H:i", strtotime($featured['date_time'])) ?>
+                        </small>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif ?>
 <!-- END FEATURED POST -->
 
 <!-- POSTS -->
-<section class="posts">
+<section class="posts <?= mysqli_num_rows($featured_result) < 1 ? 'no-featured' : '' ?>">
     <div class="container posts__container">
         <article class="post">
             <div class="post__thumbnail">
